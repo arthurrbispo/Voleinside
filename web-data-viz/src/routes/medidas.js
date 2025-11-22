372ae918635987252c2f81bf96e1fd649beb0e89
arthurrbispo <arthur.bsantos@sptech.school>
2025-11-22 20:23:22 -0300

    Configurando a Dashboard

--- web-data-viz/src/routes/medidas.js
+++ web-data-viz/src/routes/medidas.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 var medidaController = require("../controllers/medidaController");
 
-router.get("/ultimas/:idRegistro", function (req, res) {
+router.get("/ultimas/:idUsuario", function (req, res) {
     medidaController.buscarUltimasMedidas(req, res);
 });
 
